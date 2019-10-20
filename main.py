4acc9fd4def7514f72aee54baedb71c9e638d183
Ashvin Warrier <warrierar@gmail.com>
2019-10-19 23:54:54 -0400

    Updated methods

--- main.py
+++ main.py
@@ -7,37 +7,37 @@ class App:
         self._display_surf = None
         self.size = self.weight, self.height = 640, 400
 
-    def on_init(self):
+    def init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
         return True
 
-    def on_event(self, event):
+    def handle_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
 
-    def on_loop(self):
+    def loop(self):
         pass
 
-    def on_render(self):
+    def render(self):
         pass
 
-    def on_cleanup(self):
+    def cleanup(self):
         pygame.quit()
 
-    def on_execute(self):
-        if not self.on_init():
+    def execute(self):
+        if not self.init():
             self._running = False
 
         while self._running:
             for event in pygame.event.get():
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
+                self.handle_event(event)
+            self.loop()
+            self.render()
+        self.cleanup()
 
 
 if __name__ == "__main__":
-    theApp = App()
-    theApp.on_execute()
+    app = App()
+    app.execute()
